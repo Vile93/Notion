254989325b2faf8291eafefeff5a1c860bdc84c3
Vile <vilemusicyt@gmail.com>
2024-11-22 10:18:08 -0700

    🚀 feat(client): implement CustomInput component
    - create CustomInput component for customizable input fields
    - utilize react-hook-form for form control
    - handle field validation and error messages
    - support different input types and styling options
    
    🔧 chore(constants): add user-related constants and schemas
    
    - define placeholder constants for username, email, password, and repeat password
    - create UserRegisterSchema and UserLoginSchema for user registration and login
    - add validation logic for password matching in UserRegisterSchema

---
+++ app/client/src/components/CustomInput.tsx
@@ -0,0 +1,49 @@
+import { FC } from 'react';
+import { Input } from '@mui/material';
+import { Control, Controller } from 'react-hook-form';
+
+interface ICustomInputProps {
+    control: Control<any>;
+    name: string;
+    placeholder?: string;
+    className?: string;
+    type?: string;
+}
+
+const CustomInput: FC<ICustomInputProps> = ({
+    control,
+    placeholder,
+    name,
+    className,
+    type,
+}) => {
+    return (
+        <Controller
+            name={name}
+            control={control}
+            render={({ field, fieldState }) => (
+                <div>
+                    <Input
+                        type={type ?? 'text'}
+                        color={
+                            fieldState?.error?.message && fieldState.isTouched
+                                ? 'error'
+                                : 'primary'
+                        }
+                        placeholder={placeholder ?? ''}
+                        className={className ?? ''}
+                        {...field}
+                    />
+                    {(fieldState?.error?.message && fieldState?.isTouched) ||
+                    fieldState.invalid ? (
+                        <div className="text-red-600 text-sm">
+                            {fieldState?.error?.message}
+                        </div>
+                    ) : null}
+                </div>
+            )}
+        />
+    );
+};
+
+export default CustomInput;
--- app/client/src/constants.tsx
+++ app/client/src/constants.tsx
@@ -1,6 +1,22 @@
+import { UserSchema } from '../../shared/src/Schemes/UserSchema';
+import { z } from 'zod';
+
+// eslint-disable-next-line react-refresh/only-export-components
 export const PLACEHOLDRS = {
     USERNAME: 'Username',
     EMAIL: 'Email',
     PASSWORD: 'Passowrd',
     REPEAT_PASSWORD: 'Repeat password',
 };
+
+export const UserRegisterSchema = UserSchema.extend({
+    repeatPassword: z.string(),
+}).refine((data) => data.password === data.repeatPassword, {
+    message: 'Passwords do not match.',
+    path: ['repeatPassword'],
+});
+
+export const UserLoginSchema = UserSchema.pick({
+    email: true,
+    password: true,
+});
