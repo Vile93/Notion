1ea6ca2046e8540e4c13e75d66cb733952ebcb2f
Vile <vilemusicyt@gmail.com>
2024-11-24 08:25:12 -0700

    ✨ feat(client): add custom hook for modal functionality
    - create a new custom hook 'useModal' for handling modal state
    - includes functions for opening and closing the modal
    - returns state and functions for modal interaction

---
+++ app/client/src/hooks/useModal.ts
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+const useModal = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const closeModal = () => setIsOpen(false);
+    const openModal = () => setIsOpen(true);
+    return {
+        isOpen,
+        closeModal,
+        openModal,
+    };
+};
+
+export default useModal;
