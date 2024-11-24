96135e235adb2a7b47221df94ef3c4d0c8feda91
Vile <vilemusicyt@gmail.com>
2024-11-24 03:28:46 -0700

    🔧 chore(client): refactor login and register page components
    - replace useState with useContext and useEffect for authentication context usage
    - replace useQuery with custom useFetch hook for data fetching
    - simplify onSubmit function with async/await for data fetching
    - update useEffect dependency array for auth state and fetch status in both components

--- app/client/src/pages/Loginpage.tsx
+++ app/client/src/pages/Loginpage.tsx
@@ -4,27 +4,14 @@ import { PLACEHOLDRS, UserLoginSchema } from '../constants';
 import CustomInput from '../components/CustomInput';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { IUserLogin } from '../interfaces/IUserLogin';
-import { useState } from 'react';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useContext, useEffect } from 'react';
 import { loginUser } from '../services/auth.service';
-import { saveJWT } from '../utils/saveJWT';
+import { UserContext } from '../App';
+import useFetch from '../hooks/useFetch';
 
 const Loginpage = () => {
-    const queryClient = useQueryClient();
-    const [enabled, setEnabled] = useState(false);
-    const [candidate, setCandidate] = useState<IUserLogin>({
-        password: '',
-        email: '',
-    });
-    const { data, isSuccess } = useQuery({
-        queryKey: ['login'],
-        queryFn: () => {
-            setEnabled(false);
-            return loginUser(candidate);
-        },
-        enabled,
-    });
-
+    const auth = useContext(UserContext);
+    const authFetch = useFetch(false, !!auth?.isAuth);
     const { control, handleSubmit } = useForm({
         defaultValues: {
             password: '',
@@ -33,21 +20,16 @@ const Loginpage = () => {
         mode: 'onSubmit',
         resolver: zodResolver(UserLoginSchema),
     });
-
-    const onSubmit = (data: IUserLogin) => {
-        setEnabled(true);
-        setCandidate(data);
+    const onSubmit = async (data: IUserLogin) => {
+        authFetch.fetchData(loginUser, { ...data });
     };
 
-    if (isSuccess) {
-        const { token } = data;
-        if (token) {
-            saveJWT(data.token);
-            queryClient.setQueryData(['jwt'], () => ({
-                token: localStorage.getItem('jwt'),
-            }));
+    useEffect(() => {
+        if (auth?.setIsAuth && authFetch.isAuth) {
+            auth.setIsAuth(true);
         }
-    }
+    }, [auth, authFetch.isAuth]);
+
     return (
         <div className="flex flex-col items-center">
             <h1 className="title">Log in</h1>
@@ -66,8 +48,8 @@ const Loginpage = () => {
                     className="w-full"
                 />
                 <Button type="submit">Log in</Button>
-                {data?.message ? (
-                    <div className="error">{data.message}</div>
+                {authFetch?.data?.message ? (
+                    <div className="error">{authFetch.data?.message}</div>
                 ) : null}
             </form>
         </div>
--- app/client/src/pages/Registerpage.tsx
+++ app/client/src/pages/Registerpage.tsx
@@ -4,28 +4,14 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import CustomInput from '../components/CustomInput';
 import { IUserRegister } from '../interfaces/IUserRegister';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { registerUser } from '../services/auth.service';
-import { useState } from 'react';
-import { saveJWT } from '../utils/saveJWT';
+import { useContext, useEffect } from 'react';
+import { UserContext } from '../App';
+import useFetch from '../hooks/useFetch';
 
 const Registerpage = () => {
-    const queryClient = useQueryClient();
-    const [enabled, setEnabled] = useState(false);
-    const [candidate, setCandidate] = useState<IUserRegister>({
-        username: '',
-        password: '',
-        repeatPassword: '',
-        email: '',
-    });
-    const { data, isSuccess } = useQuery({
-        queryKey: ['register'],
-        queryFn: () => {
-            setEnabled(false);
-            return registerUser(candidate);
-        },
-        enabled,
-    });
+    const auth = useContext(UserContext);
+    const authFetch = useFetch(false, !!auth?.isAuth);
 
     const { control, handleSubmit } = useForm({
         resolver: zodResolver(UserRegisterSchema),
@@ -37,19 +23,16 @@ const Registerpage = () => {
         },
         mode: 'onChange',
     });
-    const onSubmit = (data: IUserRegister) => {
-        setEnabled(true);
-        setCandidate(data);
+    const onSubmit = async (data: IUserRegister) => {
+        authFetch.fetchData(registerUser, { ...data });
     };
-    if (isSuccess) {
-        const { token } = data;
-        if (token) {
-            saveJWT(data.token);
-            queryClient.setQueryData(['jwt'], () => ({
-                token: localStorage.getItem('jwt'),
-            }));
+
+    useEffect(() => {
+        if (auth?.setIsAuth && authFetch.isAuth) {
+            auth.setIsAuth(true);
         }
-    }
+    }, [auth, authFetch.isAuth]);
+
     return (
         <div className="flex flex-col items-center">
             <h1 className="title">Sign up</h1>
@@ -81,8 +64,8 @@ const Registerpage = () => {
                     className="w-full"
                 />
                 <Button type="submit">Sign up</Button>
-                {data?.message ? (
-                    <div className="error">{data.message}</div>
+                {authFetch.data?.message ? (
+                    <div className="error">{authFetch.data?.message}</div>
                 ) : null}
             </form>
         </div>
