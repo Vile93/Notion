b0f76e83e185beffb08f0cecbe3b4c360d7a8137
Vile <vilemusicyt@gmail.com>
2024-11-24 14:42:16 -0700

    🔧 chore(utils): remove clearJWT and fetchOptions utility functions
    - delete clearJWT utility function as it is no longer needed
    - remove fetchOptions utility function for request configurations to streamline codebase efficiency

--- app/client/src/utils/fetchOptions.ts
+++
@@ -1,9 +0,0 @@
-export const fetchOptions = () => {
-    return {
-        headers: {
-            Authorization: localStorage.getItem('jwt') ?? '',
-            'Content-Type': 'application/json',
-        },
-        credentials: 'include',
-    };
-};
