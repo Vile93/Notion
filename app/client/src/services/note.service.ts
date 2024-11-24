d6daa3f5535a0e809e45a17175aec58e9f7f8014
Vile <vilemusicyt@gmail.com>
2024-11-24 05:09:02 -0700

    🚀 feat(service): add editNote function
    - implement editNote function to update existing note data
    - make a PUT request to the API endpoint with new note data for the specified ID

--- app/client/src/services/note.service.ts
+++ app/client/src/services/note.service.ts
@@ -21,3 +21,11 @@ export const deleteNote = (id: string) => {
         ...(fetchOptions() as RequestInit),
     }).then((res) => res.json());
 };
+
+export const editNote = (id: string, newNoteData: INote) => {
+    return fetch(import.meta.env.VITE_API + `/notes/${id}`, {
+        body: JSON.stringify(newNoteData),
+        method: 'PUT',
+        ...(fetchOptions() as RequestInit),
+    }).then((res) => res.json());
+};
