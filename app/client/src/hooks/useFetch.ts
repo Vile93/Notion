9d9caa1668b55a3623874af8b4b2fccccb906878
Vile <vilemusicyt@gmail.com>
2024-11-24 14:42:39 -0700

    ♻️ refactor(useFetch): improve error handling and data mutation
    - extract error handling logic into a separate function (errorHandler)
    - utilize try-catch blocks for better error control and handling
    - update fetchData function to handle different cases based on isVerification flag
    - store error object to provide more detailed error information
    - create mutateData function to set new data and trigger re-render

--- app/client/src/hooks/useFetch.ts
+++ app/client/src/hooks/useFetch.ts
@@ -1,52 +1,79 @@
 import { useState } from 'react';
 import { createJWT } from '../services/auth.service';
 import { saveJWT } from '../utils/saveJWT';
-import { clearJWT } from '../utils/clearJWT';
+import axios from 'axios';
 
 const useFetch = <T extends any[]>(
     isVerification: boolean,
-    wasAuthorized: boolean
+    setAuth?: React.Dispatch<React.SetStateAction<boolean>>
 ) => {
     const [data, setData] = useState<any | null>(null);
-    const [isAuth, setIsAuth] = useState<boolean>(wasAuthorized);
     const [isLoading, setIsLoading] = useState(false);
     const [isCompleted, setIsCompleted] = useState(false);
+    const [error, setError] = useState({
+        status: false,
+        message: '',
+    });
+    const errorHandler = (err: Error) => {
+        if (axios.isAxiosError(err)) {
+            if (err?.status === 401) {
+                if (setAuth) setAuth(false);
+            }
+            setError({
+                status: true,
+                message: err.response?.data?.message,
+            });
+        }
+    };
     const fetchJWT = async () => {
         const jwt = await createJWT();
-        console.log(jwt);
+        console.log('jwt', jwt);
 
-        if (!jwt?.token) {
-            setIsAuth(false);
-            clearJWT();
-        } else {
-            setIsAuth(true);
-            saveJWT(jwt.token);
+        try {
+            saveJWT(jwt?.data?.token);
+            if (setAuth) setAuth(true);
+        } catch (err) {
+            errorHandler(err as Error);
         }
     };
     const fetchData = async (cb: (...args: T) => any, ...args: T) => {
         setIsLoading(true);
         setIsCompleted(false);
+        setError({
+            status: false,
+            message: '',
+        });
         let res = null;
-        if (isVerification) {
-            await fetchJWT();
-            res = await cb(...args);
+        if (!isVerification) {
+            try {
+                res = await cb(...args);
+                if (res.data?.token && setAuth) {
+                    setAuth(true);
+                }
+            } catch (err) {
+                errorHandler(err as Error);
+            }
         } else {
-            res = await cb(...args);
-            if (res?.token) {
-                setIsAuth(true);
+            try {
+                await fetchJWT();
+                res = await cb(...args);
+                if (setAuth) setAuth(true);
+            } catch (err) {
+                errorHandler(err as Error);
             }
         }
-        setData(res);
+        setData(res?.data);
         setIsLoading(false);
         setIsCompleted(true);
     };
 
     return {
-        isAuth,
         isCompleted,
         data,
+        error,
         isLoading,
         fetchData,
+        mutateData: setData,
     };
 };
 
