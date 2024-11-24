c7bde8e270b3f56b237c2bbafaca52c6674989da
Vile <vilemusicyt@gmail.com>
2024-11-24 07:16:08 -0700

    🐛 fix(models): remove unnecessary 'required' constraint for note text field
    - update noteSchema to make the 'text' field not required to allow empty notes being saved

--- app/server/src/models/noteSchema.ts
+++ app/server/src/models/noteSchema.ts
@@ -3,7 +3,7 @@ import { model, Schema } from 'mongoose';
 const noteSchema = new Schema(
     {
         title: { type: String, required: true },
-        text: { type: String, required: true },
+        text: { type: String },
         userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     },
     {
