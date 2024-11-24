c4ddefb54a1e0e6a605777469a583fbc7d420db1
Vile <vilemusicyt@gmail.com>
2024-11-24 05:08:53 -0700

    🔥 remove(client): remove unused QueryClient setup
    - remove QueryClient and QueryClientProvider setup as it is no longer used for data fetching
    - simplify the component rendering structure to remove unnecessary nesting

--- app/client/src/main.tsx
+++ app/client/src/main.tsx
@@ -2,23 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import UserProvider from './contexts/UserContext';
-
-const client = new QueryClient({
-    defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-        },
-    },
-});
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <QueryClientProvider client={client}>
-            <UserProvider>
-                <App />
-            </UserProvider>
-        </QueryClientProvider>
+        <UserProvider>
+            <App />
+        </UserProvider>
     </StrictMode>
 );
