75996a601e33217a69c89f22aefc7193f73d994b
Vile <vilemusicyt@gmail.com>
2024-12-06 17:11:08 -0700

    ♻️ refactor(user): improve user actions and reducer structure
    - replace USER_ACTIONS object with enum for better type safety
    - reorganize user actions with more descriptive types
    - enhance user reducer readability with clearer action mapping techniques

--- app/client/src/store/userReducer/userReducer.ts
+++ app/client/src/store/userReducer/userReducer.ts
@@ -1,14 +1,22 @@
 import { IUser } from '../../interfaces/IUser';
 import { USER_ACTIONS } from './userActions';
 
-export type UserStore = {
+type UserStore = {
     isAuth: boolean;
     data: IUser | null;
 };
 
-type Action = { type: typeof USER_ACTIONS.SET_NEW_USER; payload: UserStore } & {
-    type: typeof USER_ACTIONS.CLEAR_USER_DATA;
-};
+export type Action =
+    | {
+          type: typeof USER_ACTIONS.SET_DATA;
+          payload: UserStore;
+      }
+    | {
+          type: typeof USER_ACTIONS.CLEAR_USER_DATA;
+      }
+    | { type: typeof USER_ACTIONS.CHANGE_AUTH; payload: boolean }
+    | { type: typeof USER_ACTIONS.AUTH }
+    | { type: typeof USER_ACTIONS.UNAUTH };
 
 const initialState: UserStore = {
     isAuth: !!localStorage.getItem('jwt'),
@@ -17,13 +25,25 @@ const initialState: UserStore = {
 
 export function userReducer(state = initialState, action: Action): UserStore {
     switch (action.type) {
-        case USER_ACTIONS.SET_NEW_USER:
-            return { ...state, ...action.payload };
+        case USER_ACTIONS.SET_DATA:
+            return {
+                ...state,
+                data: {
+                    ...action.payload,
+                },
+            };
         case USER_ACTIONS.CLEAR_USER_DATA:
             return {
                 isAuth: false,
                 data: null,
             };
+        case USER_ACTIONS.CHANGE_AUTH:
+            return { ...state, isAuth: action.payload };
+        case USER_ACTIONS.AUTH:
+            return { ...state, isAuth: true };
+        case USER_ACTIONS.UNAUTH:
+            return { ...state, isAuth: false };
+
         default:
             return state;
     }
